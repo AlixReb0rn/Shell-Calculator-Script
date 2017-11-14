299f5d8f814de978155693bd0e44cc860069aefa
AlixReb0rn <32742967+AlixReb0rn@users.noreply.github.com>
2017-11-14 20:31:27 +0200

    Add files via upload

---
+++ ShellCalculator.sh
@@ -0,0 +1,9 @@
+echo "Welcome! to Calculator Coded By Alix Reb0rn"
+echo "---------------------******-----------------------"
+echo "Type The Value's one by one that you want to calculate and add (+/-/x)"
+read Value1
+read WayCalc
+read Value2
+echo "And I Think I Found the answer. : "
+expr $Value1 $WayCalc $Value2
+read FuckOff
